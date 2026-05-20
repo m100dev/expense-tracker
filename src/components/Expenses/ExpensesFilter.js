2b80cb6fde6ba32caf63e28c41fb1ec49fd45b12
Marc Bastien <117379255+m100dev@users.noreply.github.com>
2026-05-20 04:18:21 -0400

    Update ExpensesFilter.js to include more years

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -17,6 +17,9 @@ const ExpensesFilter = (props) => {
                     <option value='2021'>2021</option>
                     <option value='2022'>2022</option>
                     <option value='2023'>2023</option>
+                    <option value='2023'>2024</option>
+                    <option value='2023'>2025</option>    
+                    <option value='2023'>2026</option>
                 </select>
             </div>
         </div>
